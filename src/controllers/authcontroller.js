4cf1e1c184ce9477899ef22549c59c35f4b8b123
ichrystm <ichrystm@gmail.com>
2020-05-13 19:23:31 -0300

    APILoginJS v1.1

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const mid = require('../middlewares/cors');
 
@@ -12,20 +13,41 @@ router.post('/register', async (req, res) => {
     const { email } = req.body;
 
     try {
-        if (await User.findOne({ email })) {
+        if (await User.findOne({ email }))
             return status.send(400).send({ error: 'User already exists' });
-            } else {
         
             const user = await User.create(req.body);
 
             user.password = undefined;
 
-            return res.send({ user });
 
-            }
+        return res.status(200).send({ user });
+
     } catch (err) {
-        return res.status(400).send({ error: 'Registration failed' });
+
+        return res.status(401).send({ error: 'Registration failed' });
+
     }
 });
+
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user)
+    return res.status(404).send({ error: 'User not found' });
+
+    if (!await bcrypt.compare(password, user.password))
+    return res.status(401).send({ error: 'Invalid Password' });
+
+    res.status(200).send('Login sucessfull');
+    
+
+})
+
+
+
 
 module.exports = app => app.use('/auth', router);
--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -11,5 +11,4 @@ module.exports = (req, res, next) => {
     app.use(cors());
     next();
 
-    //console.log("Middleware!");
 }
