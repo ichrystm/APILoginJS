d3a298275ae8e087f2dd83cef0703778064048e5
ichrystm <ichrystm@gmail.com>
2020-05-13 19:21:30 -0300

    authcontroller

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -9,20 +9,20 @@ router.use(mid);
 
 router.post('/register', async (req, res) => {
 
-
-
     const { email } = req.body;
 
     try {
-        if (await User.findOne({ email }))
+        if (await User.findOne({ email })) {
             return status.send(400).send({ error: 'User already exists' });
+            } else {
         
             const user = await User.create(req.body);
 
             user.password = undefined;
 
+            return res.send({ user });
 
-        return res.send({ user });
+            }
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
